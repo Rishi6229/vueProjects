e8e18c273cefabcf3568230d4e8015fd4c2c2694
Rishi6229 <rk6180442@gmail.com>
2023-09-26 12:37:48 +0530

    colorFlipper:

---
+++ colorFlipperApp/vue-project/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ColorFlipper />
+  </div>
+</template>
+
+<script> 
+import ColorFlipper from './components/ColorFlipper.vue';
+export default{
+  components:{
+    ColorFlipper,
+  }
+}
+</script>
+<style> 
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ colorFlipperApp/vue-project/src/components/ColorFlipper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="color-flipper">
+    <button @click="changeColor">{{ buttonText }}</button>
+    <div :style="{ backgroundColor }" class="color-box">
+      <p>{{ backgroundColor }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      backgroundColor: "",
+      buttonText:'Change Color',
+    };
+  },
+  methods: {
+    changeColor() {
+      this.backgroundColor = getRandomColor();
+      this.buttonText = getRandomColor();
+    },
+  },
+};
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+</script>
+<style>
+.color-flipper {
+  text-align: center;
+}
+.color-box {
+  width: 200px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 24px;
+  text-transform: uppercase;
+}
+</style>
