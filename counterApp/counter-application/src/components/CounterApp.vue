de2c3796c936e4999b6e78cbf4c9b11c76d2f56d
Rishi6229 <rk6180442@gmail.com>
2023-09-28 11:17:25 +0530

    made slight modifications

--- counterApp/counter-application/src/components/CounterApp.vue
+++ counterApp/counter-application/src/components/CounterApp.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="counter-app">
-    <p>{{ InitialCount }}</p>
     <section class="button-section">
       <button @click="incrementCounter">Add</button>
       <button @click="decrementCounter">Substract</button>
@@ -18,10 +17,10 @@ export default{
   },
   methods: {
     incrementCounter() {
-      this.InitialCount = this.count + 10;
+      this.count = this.InitialCount + 10;
     },
     decrementCounter() {
-        this.InitialCount = this.count - 10;
+        this.count = this.InitialCount - 10;
     },
   },
 };
@@ -33,17 +32,30 @@ export default{
     display: flex;
     justify-content: space-evenly;
 }
-
+.inputField{
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+    padding: 20px;
+    border-radius: 20px;
+}
 button{
     height: 100px;
     width: 100px;
     border: 5px solid black;
     border-radius: 10px;
+
 }
 p{
     
     display: flex;
     justify-content: center;
+}
+
+.counter-app{
+    display: flex;
+    justify-content: center;
+    top:0px;
 }
 
 
